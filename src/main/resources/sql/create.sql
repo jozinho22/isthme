feafd5bb2e13405ad64fbdb6a2d4f30d57d218ed
jozinho22 <josselin.douineau.1987@gmail.com>
2020-05-20 16:52:00 +0200

    retrait codes

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -6,14 +6,6 @@ CREATE TABLE avis  (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO avis (author, quote, to_validate) VALUES ("Socrate", "Tout ce que je sais...", "1");
-INSERT INTO avis (author, quote, to_validate) VALUES ("Coubertin", "L'important c'est de participer...", "1");
-INSERT INTO avis (author, quote, to_validate) VALUES ("La fontaine", "On a toujours besoin d'un...", "1");
-INSERT INTO avis (author, quote, to_validate) VALUES ("Descartes", "Je pense donc je suis...", "1");
-INSERT INTO avis (author, quote, to_validate) VALUES ("Einstein", "Il est plus facile de briser un atome...", "1");
-INSERT INTO avis (author, quote, to_validate) VALUES ("La fontaine", "Rien ne sert de courir...", "1");
-
-
 CREATE TABLE IF NOT EXISTS client (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
